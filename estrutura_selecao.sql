7c27675122b809dc348f05e551051399997e259c
Iara <iara.silva14@fatec.sp.gov.br>
2023-09-12 09:03:57 -0300

    ap09(selecao): exemplos de if

--- estrutura_selecao.sql
+++ estrutura_selecao.sql
@@ -1,7 +1,21 @@
-CREATE OR REPLACE FUNCTION fn_valor_aleatorio_entre
-(lim_inferior INT, lim_superior INT) RETURNS INT AS
+DO
 $$
+DECLARE
+	valor INT;
 BEGIN
-	RETURN FLOOR(random() * (lim_inferior - lim_superior +1) + lim_inferior)::INT;
-END
-$$ LANGUAGE plpgsql;
+	valor := fn_valor_aleatorio_entre(1,10);
+	RAISE NOTICE 'O valor gerado foi %', valor;
+		IF valor <= 20 THEN
+			RAISE NOTICE 'Sim, % é menor ou igual a 20', valor;	
+		END IF;
+	   END;
+$$
+-- SELECT fn_valor_aleatorio_entre(2,5);
+
+-- CREATE OR REPLACE FUNCTION fn_valor_aleatorio_entre
+-- (lim_inferior INT, lim_superior INT) RETURNS INT AS
+-- $$
+-- BEGIN
+-- 	RETURN FLOOR(random() * (lim_inferior - lim_superior +1) + lim_inferior)::INT;
+-- END
+-- $$ LANGUAGE plpgsql;
